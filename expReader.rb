b754178fe57ee4612792be7a461cc28abfeac38e
Andrey Inishev <inish777@gmail.com>
2013-11-18 20:32:26 +0700

    Shitcode removal

--- expReader.rb
+++ expReader.rb
@@ -1,127 +1,94 @@
-expin = gets().to_s
+# -*- coding: utf-8 -*-
+class NotAnOperator < Exception
+end
 
-def String.clear()
-  self.split("").clear
+class Operator
+  @@operators = {"+" => :left_associative, "-" => :left_associative, \
+    "*" => :left_associative, "/" => :left_associative, "^" => :right_associative}
+  
+  def initialize(char)
+    if @@operators.include?(char)
+      @operator = char
+      @association = @@operators[char]
+    else
+      raise NotAnOperator.new("Unknown operator: #{char}")
+    end
+  end
+
+  def self.is_operator?(char)
+    return true if @@operators.include?(char) else false
+  end
+ 
+  def association
+    @association
+  end
+end
+
+class Function
+  functions = {"sin" => :fd}
+end
+
+class Expression
+  def initialize(exp)
+    @exp = exp.chomp.delete " "
+    @stack = []
+    @output = ""
+  end
+
+  def to_rpn
+    p @exp
+    index = 0
+    while @exp[index] != nil
+      x = tokenize(index)
+      p x.to_s
+      case x[:type]
+      when :operator
+        @stack << x[:token]
+      when :num
+        @output << x[:token]
+      else
+        p "Unknown tokenize() result"
+      end
+      index = x[:index]
+    end
+
+    p "#{@stack.to_s} - #{@output}"
+  end
+  
+  private
+  def tokenize(index)
+    buf = ""
+    i = index
+
+    buf << @exp[i]
+    i += 1
+    if Operator.is_operator?(buf)
+      return {:token => Operator.new(buf), :type => :operator, :index => i}
+    end
+
+    if Integer(buf)
+      until Operator.is_operator?(@exp[i]) or @exp[i] == nil
+        buf << @exp[i]
+        i += 1
+      end
+      return {:token => Integer(buf), :type => :num,:index => i}
+    end
+  end
 end
 
 class Calc
 
   def initialize(exp = nil)
-    @exp = exp
-    @expout, @problem = "", ""
-    @stack, @problems, @problemPoss = [], [], []
-    @problemPos = 0
-    @funcs, @digits = [], []
+    @expout = ""
+    @stack = []
   end
-  
-  def is_a_letter?(object)
-    false if (object == ".")
-    true if String(object) rescue false
+ 
+  def run
+    exp = gets.to_s
+    expression = Expression.new(exp)
+    expression.to_rpn
   end
-  
-  def is_a_digit?(object)
-    false if (object == "-")
-    true if Float(object) rescue false
-  end
-
-  def is_a_function?(string)
-    funcs = ["sin", "cos", "+", "-", "/", "*"]
-    if funcs.include?(string) then
-      true
-    else
-      false
-    end
-  end
-
-  def update_problems
-    @problems << @problem
-    @problem.clear
-    @problemPoss << @problemPos
-  end
-  
-  def to_rpn
-    #return nil if !(String(@exp))
-    
-    @exp.chomp!
-    @exp.delete! " "
-    @exp += " "
-    index = 0
-    buf = ""
-    @exp.split("").each do |sym|
-      index += 1
-      if is_a_function?(sym) then
-        #поймали функцию
-        @funcs << sym
-        if is_a_digit?(buf) then
-          #поймали число
-          buf.split("").pop
-          @digits << buf
-        end
-        buf.clear
-        #отладка
-        update_problems
-        next
-      end
-      if is_a_function?(buf) then
-        #поймали функцию
-        @funcs << buf
-        buf.clear
-        #отладка
-        update_problems
-        next
-      else
-        #буфер не является функцией 
-        if buf != " " then
-          @problemPos = index - buf.length
-          @problem += sym
-        end
-        if is_a_digit?(buf) then
-          #отладка
-          update_problems
-          #в буфере неопознанная функция перебилась цифрой
-          buf = buf.split("").pop
-          next
-        end        
-=begin
-          if is_a_letter?(sym) then
-            #поймали число
-            buf.split("")[index] = nil
-            @digits << buf
-            buf.clear
-            buf += sym
-            #отладка
-            update_problems
-            next
-          end
-          
-          #поймали число
-          buf += sym
-          buf.split("")[index] = nil
-          @digits << buf
-          buf.clear
-          #отладка
-          update_problems
-          next
-=end
-      end
-    end
-  
-    return @expout
-  end
-  
-  def inf
-    print "\nFunctions:\n"
-    @funcs.each {|func| print "#{@func} "}
-    print "\nNumbers:\n"
-    @digits.each {|digit| print "#{@digit} "}
-    puts
-  end
-  
 end
 
-calc = Calc.new(expin)
-calc.to_rpn
-calc.inf
-
-puts
-
+calc = Calc.new
+calc.run
